c6d376993c33b5fc4b8a8253ba886e6663a47fa6
Holland Graves <zacharyledoux13@gmail.com>
2018-10-25 20:47:57 -0600

    added a few more notes on interfaces

--- notes.go
+++ notes.go
@@ -845,6 +845,31 @@ embedding interfaces
 
 
 
+GO NOTE:
+the make function
+
+	// the make() function is a function that can initialize a slice, map, or channel with a set integer of empty spaces
+
+			// The make built-in function allocates and initializes an object of type
+			// slice, map, or chan (only). Like new, the first argument is a type, not a
+			// value. Unlike new, make's return type is the same as the type of its
+			// argument, not a pointer to it. The specification of the result depends on
+			// the type:
+			//		Slice: The size specifies the length. The capacity of the slice is
+			//		equal to its length. A second integer argument may be provided to
+			//		specify a different capacity; it must be no smaller than the
+			//		length. For example, make([]int, 0, 10) allocates an underlying array
+			//		of size 10 and returns a slice of length 0 and capacity 10 that is
+			//		backed by this underlying array.
+			//		Map: An empty map is allocated with enough space to hold the
+			//		specified number of elements. The size may be omitted, in which case
+			//		a small starting size is allocated.
+			//		Channel: The channel's buffer is initialized with the specified
+			//		buffer capacity. If zero, or the size is omitted, the channel is
+			//		unbuffered.
+
+
+
 */
 
 func main() {}
